5be25b197c1595a256f1a5bb4931ee825c39428e
13141391899 <13141391899@163.com>
2020-12-16 14:57:16 +0800

    初始化

---
+++ src/main/java/com/zjh/fangdichanhaixia/FangdichanhaixiaApplication.java
@@ -0,0 +1,13 @@
+package com.zjh.fangdichanhaixia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FangdichanhaixiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FangdichanhaixiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjh/fangdichanhaixia/FangdichanhaixiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.fangdichanhaixia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FangdichanhaixiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
